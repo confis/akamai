352a74c700e7e0ee86a670e61f4c4df4f34b69fd
Yaniv Cohen <confis@gmail.com>
2022-02-02 19:58:35 +0200

    Update addcrt.sh

--- script/addcrt.sh
+++ script/addcrt.sh
@@ -7,10 +7,10 @@ cat >> .edgerc << 'END'
 ${{ secrets.AKAMAI_TOKEN }}
 END
 
-File="/domain/domainlist.txt"
+File="domain/domainlist.txt"
 while IFS= read -r line
 do
-  cat sample.yml | sed 's/foo.com/'$line'/g' > /crt/$line.yml
-  # akamai cps create --file /crt/$line.yml --contract-id V-40T9211 --force --section default
+  cat sample.yml | sed 's/foo.com/'$line'/g' > crt/$line.yml
+  # akamai cps create --file crt/$line.yml --contract-id V-40T9211 --force --section default
   echo "domain: $line"
 done < "$File"
