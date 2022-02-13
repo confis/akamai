72ca36263f242a1dccda470c012a1d1c8d4a2757
Yaniv Cohen <confis@gmail.com>
2022-02-13 17:21:35 +0200

    Update addcrt.sh

--- script/addcrt.sh
+++ script/addcrt.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-apk update
-apk upgrade	
-apk add curl
-apk add bash
 cat >> .edgerc << 'END'
 ${{ secrets.AKAMAI_TOKEN }}
 END
 
-File="domain/domainlist.txt"
-while IFS= read -r line
-do
-  cat script/sample.yml | sed 's/foo.com/'$line'/g' > crt/$line.yml
-  # akamai cps create --file crt/$line.yml --contract-id V-40T9211 --force --section default
-  echo "domain: $line"
-done < "$File"
+git add . -A
+git commit -m "$commit_message"
+git push
+
+#File="domain/domainlist.txt"
+#while IFS= read -r line
+#do
+#  cat script/sample.yml | sed 's/foo.com/'$line'/g' > crt/$line.yml
+#  # akamai cps create --file crt/$line.yml --contract-id V-40T9211 --force --section default
+#  echo "domain: $line"
+#done < "$File"
