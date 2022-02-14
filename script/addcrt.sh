4bd9744bab66d1a439af561f106bd2549f79b945
Yaniv Cohen <confis@gmail.com>
2022-02-14 11:18:22 +0200

    Update addcrt.sh

--- script/addcrt.sh
+++ script/addcrt.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-cat >> .edgerc << 'END'
-${{ secrets.AKAMAI_TOKEN }}
-END
+#cat >> .edgerc << 'END'
+#${{ secrets.AKAMAI_TOKEN }}
+#END
 
-cd ./crt
-echo "Hello" > index.foo
-git add  "index.foo"
-git commit -m "Commit message"
+#cd ./crt
+#echo "Hello" > index.foo
+#git add  "index.foo"
+#git commit -m "Commit message"
 
-#File="domain/domainlist.txt"
-#while IFS= read -r line
-#do
-#  cat script/sample.yml | sed 's/foo.com/'$line'/g' > crt/$line.yml
-#  # akamai cps create --file crt/$line.yml --contract-id V-40T9211 --force --section default
-#  echo "domain: $line"
-#done < "$File"
+File="domain/domainlist.txt"
+while IFS= read -r line
+do
+  cat ./script/sample.yml | sed 's/foo.com/'$line'/g' > echo ./crt/$line.yml
+  echo "domain: $line"
+  git add  "$line.yml"
+  git commit -m "Commit message"
+done < "$File"
