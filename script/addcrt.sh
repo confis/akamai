9dfddd042829c1e661fb4a01da63a2ea03a4cadd
Yaniv Cohen <confis@gmail.com>
2022-02-14 16:24:30 +0200

    Update addcrt.sh

--- script/addcrt.sh
+++ script/addcrt.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-
-#echo "${{ steps.sample.outputs.content }}" >  sample4.yml
-
-#cat >> .edgerc << 'END'
-#${{ secrets.AKAMAI_TOKEN }}
-#END
-
-#cd ./crt
-#echo "Hello" > index.foo
-#git add  "index.foo"
-#git commit -m "Commit message"
-
- File="domain/domainlist.txt"
- while IFS= read -r line
- do
-  cat ./script/sample.yml | sed 's/foo.com/'fofofofofof.com'/g' > echo sample4.yml
-#  echo "domain: $line"
-#  git add  "$line.yml"
-#  git commit -m "Commit message"
- done < "$File"
+File="domainList.txt"
+while IFS= read -r line
+do
+  cat sample.yml | sed 's/foo.com/'$line'/g' > ./crt/$line.yml
+ # akamai cps create --file /users/yanivcohen/redirect.folloze.com/crt/$line.yml --contract-id V-40T9211 --force --section default
+  echo "domain: $line"
+done < "$File"
