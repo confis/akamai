383a2b1c003403c5e0aa6a7f25c2abad4f690e5b
Yaniv Cohen <confis@gmail.com>
2022-02-14 16:50:29 +0200

    Update addcrt.sh

--- script/addcrt.sh
+++ script/addcrt.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -x			# activate debugging from here
+
   cat ./script/sample.yml | sed 's/foo.com/'$DOMAIN_NAME'/g' > ./crt/$DOMAIN_NAME.yml
   echo $DOMAIN_NAME.yml > $DOMAIN_NAME.yml
   git add  "$DOMAIN_NAME.yml"
   git commit -m "Commit message"  
     
+set +x			# stop debugging from here
