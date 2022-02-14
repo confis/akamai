a8fc384e8508eb5ea8b006ed416907e6d1e25841
Yaniv Cohen <confis@gmail.com>
2022-02-14 12:45:21 +0200

    Update addcrt.sh

--- script/addcrt.sh
+++ script/addcrt.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo "${{ steps.sample.outputs.content }}" >  sample4.yml
+
 #cat >> .edgerc << 'END'
 #${{ secrets.AKAMAI_TOKEN }}
 #END
@@ -8,11 +11,11 @@
 #git add  "index.foo"
 #git commit -m "Commit message"
 
-File="domain/domainlist.txt"
-while IFS= read -r line
-do
-  cat ./script/sample.yml | sed 's/foo.com/'$line'/g' > echo ./crt/$line.yml
-  echo "domain: $line"
-  git add  "$line.yml"
-  git commit -m "Commit message"
-done < "$File"
+# File="domain/domainlist.txt"
+# while IFS= read -r line
+# do
+#  cat ./script/sample.yml | sed 's/foo.com/'$line'/g' > echo ./crt/$line.yml
+#  echo "domain: $line"
+#  git add  "$line.yml"
+3  git commit -m "Commit message"
+# done < "$File"
